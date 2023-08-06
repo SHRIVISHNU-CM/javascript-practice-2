d22bfba7ac5aae60b13fc888fa366d0a462c76fc
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2023-08-06 15:54:23 +0530

    Checking username using function

---
+++ assign1.js
@@ -0,0 +1,11 @@
+function isuserpresent(a){
+    if(a == "Mithun"){
+        console.log("Yes,Mithun is valid user");
+
+    }else{
+        console.log("No,Someone is not valid user");
+    }
+
+}
+
+const result = isuserpresent("Mithun");
