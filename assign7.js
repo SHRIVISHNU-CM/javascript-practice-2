1140aab567e75559ceb72db248607a1a60e034a7
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2023-08-06 17:20:10 +0530

    generating otp

---
+++ assign7.js
@@ -0,0 +1,11 @@
+
+function generateotp(){
+    let otp="";
+    for (let i = 0; i<4; i++) {
+        otp +=Math.floor(Math.random()*10);
+        
+    }
+    return otp;
+}
+let result = generateotp();
+console.log(result);
