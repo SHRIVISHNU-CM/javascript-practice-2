bfeb59e5f711916383732b322f2f85f2854cc5a6
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2023-08-06 17:10:22 +0530

    objects

---
+++ assign6.js
@@ -0,0 +1,11 @@
+const products={
+    name:"apple 2020 macbook air laptop",
+    price:82000,
+    color:"grey",
+    harddisk:"256GB"
+};
+console.log("Below are the product details:");
+console.log("name:" +products.name);
+console.log("price:" + products.price);
+console.log("color:" + products.color);
+console.log("hardisk:" + products.harddisk);
