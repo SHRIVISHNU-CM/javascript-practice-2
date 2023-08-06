f8f7e39b128b428b81743e5eb5e8256a7480c6ea
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2023-08-06 16:00:25 +0530

    return total sum of products

---
+++ assign2.js
@@ -0,0 +1,6 @@
+function calculateTotalCartValue(apple,orange,pineapple){
+    return apple+orange+pineapple;
+}
+
+let result= calculateTotalCartValue(125,20,30);
+console.log("total amount is:"+result);
